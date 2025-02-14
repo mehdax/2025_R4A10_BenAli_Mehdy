00d37bd49f564a30c4c2e975f208300fa113d70c
mbenali <benalimehdy@gmail.com>
2025-02-14 17:23:58 +0100

    Component Header + sous comp nav + logo

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LogoComponent } from './logo/logo.component';
+import { NavComponent } from './nav/nav.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [LogoComponent,NavComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.sass'
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/header/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.sass'
+})
+export class NavComponent {
+
+}
